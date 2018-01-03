98ddfc26aba7a700c4cec16f27c6b02940a30173
F.Homewood <fhomewood98@gmail.com>
2018-01-03 20:13:16 +0000

    changes backwards compatibility

---
+++ ProgressTracker/app/src/main/java/com/fhomewood/www/progresstracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fhomewood.www.progresstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
